9334a13b44c2cec0edbe22373e50a3279dca911a
kienle1412 <lekien.2004nek@gmail.com>
2025-07-26 13:06:44 +0700

    add memory simply

--- src/agent.py
+++ src/agent.py
@@ -6,13 +6,21 @@ class Agent():
         self.agent = self.graph.create_graph().compile()
     def run_chatbot(self):
         app = self.agent
+        state = {
+                "question": "",
+                "answer": "",
+                "summary": ""
+            }
         while True:
             user_input = input("😁  You: ")
             if user_input == "q":
                 break
-            state = {
-                "question": user_input,
-                "answer": ""
-            }
+            state["question"] = user_input
+            
             result = app.invoke(state)
+            state["answer"] = result["answer"]
+            state["summary"] = result["summary"]
             print("🐸  Bot:", result["answer"])
+            print("_____________________")
+            print(result["summary"])
+            print("_____________________")
--- src/graph.py
+++ src/graph.py
@@ -14,7 +14,7 @@ from langchain_core.output_parsers import StrOutputParser
 class State(TypedDict):
     question: str
     answer: str
-    # summary: str 
+    summary: str 
 
 class StateGraph(StateGraph[State]):
     def __init__(self, state_type: type[State]):
@@ -28,6 +28,43 @@ class StateGraph(StateGraph[State]):
         )
         self.qa_chain = RetrievalChain(self.vectorstore)
         self.search_chain = search_chain()
+
+    def router_question(self, state: State) -> str:
+        template = """
+            Hãy phân tích câu hỏi sau và trả lời xem câu hỏi này có phải tiếp nối của câu hỏi trước đó không.
+            Câu hỏi: {question}
+            Hội thoại trước đó tóm tắt: {summary}
+            Trả về **yes** nếu là tiếp nối của câu hỏi trước đó.
+            Trả về **no** nếu không phải tiếp nối của câu hỏi trước đó.
+            Lưu ý: chỉ trả về **yes** hoặc **no**.
+        """
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["question", "summary"]
+        )
+        
+        chain = prompt | self.llm | StrOutputParser()
+        result = chain.invoke({"question": state["question"], "summary": state["summary"]})
+
+        return result 
+    
+    def rewrite_question(self, state: State):
+        template = """
+            Bạn là một chuyên gia viết lại câu hỏi dựa trên ngữ cảnh hội thoại trước đó.
+            Hãy dựa vào ngữ cảnh cuộc hội thoại trước đó và câu hỏi hiện tại để viết lại câu hỏi với đầy đủ ngữ cảnh.
+            Câu hỏi: {question}
+            Tóm tắt trước đó: {summary}
+            Lưu ý: Chỉ trả về câu hỏi.Viết câu hỏi tiết kiệm token nhất có thể nhưng vẫn phải đủ ngữ cảnh.
+        """
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["question", "summary"]
+        )
+        chain = prompt | self.llm | StrOutputParser()
+        question = chain.invoke({"question": state["question"], "summary": state["summary"]})
+        state["question"] = question
+
+        return state
     
     def get_answer(self, state: State):
         question = state["question"]
@@ -58,20 +95,51 @@ class StateGraph(StateGraph[State]):
         chain = prompt | self.llm | StrOutputParser()
         return chain.invoke({"question": question, "answer": answer})
     
+    def get_summary(self, state: State):
+        template = """
+            Bạn là một chuyên gia tóm tắt cuộc hội thoại giữa người và AI.
+            Hãy tóm tắt câu hỏi và câu trả lời sau, cố gắng giữ lại những ngữ cảnh quan trọng.
+            Câu hỏi: {question}
+            Câu trả lời: {answer}
+            Ở đây cũng có tóm tắt của của phần trước đó nữa, hãy giữ lại ngữ cảnh của nó, bỏ qua nó nếu không tồn tại.
+            Tóm tắt hội thoại trước đó: {summary}
+        """
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["question", "answer", "summary"]
+        )
+
+        chain = prompt | self.llm | StrOutputParser()
+        summary = chain.invoke({"question": state["question"], "answer": state["answer"], "summary": state["summary"]})
+        state["summary"] = summary
+
+        return state
+    
     def create_graph(self):
         graph = StateGraph(State)
 
+        graph.add_node("node_rewrite_question", self.rewrite_question)
         graph.add_node("node_answer", self.get_answer)
         graph.add_node("node_search", self.get_search)
+        graph.add_node("node_summary", self.get_summary)
 
-        graph.add_edge(START, "node_answer")
+        graph.add_conditional_edges(
+            START,
+            self.router_question,
+            {
+                "yes": "node_rewrite_question",
+                "no": "node_answer"
+            }
+        )
+        graph.add_edge("node_rewrite_question", "node_answer")
         graph.add_conditional_edges(
             "node_answer",
             self.router,
             {
                 "yes": "node_search",
-                "no": END
+                "no": "node_summary"
             }
         )
-        graph.add_edge("node_search", END)
+        graph.add_edge("node_search", "node_summary")
+        graph.add_edge("node_summary", END)
         return graph
